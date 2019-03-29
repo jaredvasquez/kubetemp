8118ca9173f923ab0f266dc7d628e17db7e56cd2
Jared Vasquez <jared.vasquez@cern.ch>
2019-03-29 00:14:37 -0700

    updated

---
+++ kubetemp/__init__.py
@@ -0,0 +1,1 @@
+from .core import read_params, render_path, update_params, write_output
---
+++ kubetemp/core.py
@@ -0,0 +1,69 @@
+import json
+import os
+import sys
+
+from jinja2 import Template
+import yaml
+
+
+def _check_valid_path(path):
+    if not os.path.exists(path):
+        raise ValueError(f'Path "{path}" does not exist.')
+    if not os.path.isfile(path):
+        raise ValueError(f'Path "{path}" is not a file.')
+
+
+def _load_template(path):
+    """ Loads template object from filepath """
+    _check_valid_path(path)
+    with open(path) as _file:
+        return Template(_file.read())
+
+
+def _render_template(template, **params):
+    return template.render(**params)
+
+
+def render_path(path, **params):
+    template = _load_template(path)
+    return _render_template(template, **params)
+
+
+def _read_json(path):
+    """ Read JSON file from path """
+    if path is None:
+        return {}
+
+    _check_valid_path(path)
+    with open(path) as _file:
+        return json.load(_file)
+
+
+def _read_yaml(path):
+    """ Read YAML file from path """
+    if path is None:
+        return {}
+
+    _check_valid_path(path)
+    with open(path) as _file:
+        return yaml.load(_file)
+
+
+def read_params(path):
+    if path.endswith('.json'):
+        return _read_json(path)
+    elif path.endswith(('.yml', '.yaml')):
+        return _read_yaml(path)
+    else:
+        raise ValueError(f'Path "{path}" does not have JSON or YAML extension')
+
+
+def update_params(params_dict, json_dict):
+    if json_dict is None:
+        return
+    params_dict.update(json_dict)
+
+
+def write_output(output, path):
+    with open(path, 'w') as _file:
+        _file.write(output)
