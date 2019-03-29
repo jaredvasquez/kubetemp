11b4cfcbee9b0b557276b7582b246818f9084645
Jared Vasquez <jared.vasquez@yale.edu>
2019-03-29 10:59:07 -0700

    cleaned up package meta-data (#1)

--- setup.py
+++ setup.py
@@ -2,17 +2,28 @@ from setuptools import setup
 
 setup(
     name='kubetemp',
-    version='0.1',
-    description='Tool for generating kubernetes (k8s) manifests using Jinja templates.',
-    url='https://github.com/jgv7/kubetemp',
+    version='0.1.1',
     author='Jared Vasquez',
+    description=(
+        'Tool for generating kubernetes (k8s) manifests using Jinja templates.'
+    ),
+    url='https://github.com/jgv7/kubetemp',
     license='MIT',
-    packages=['kubetemp'],
-    scripts=['bin/kubetemp'],
     install_requires=[
         'Click>=7.0',
         'jinja2>=2.10',
-        'pyyaml'
+        'pyyaml',
+    ],
+    packages=['kubetemp'],
+    python_requires=">=3.6",
+    scripts=['bin/kubetemp'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7'
     ],
     zip_safe=False,
 )
