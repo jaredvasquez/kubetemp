86ca5a658460517a119c420b6078594118e97e79
Jared Vasquez <jared.vasquez@cern.ch>
2019-03-29 15:34:02 -0700

    increment version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='kubetemp',
-    version='0.2.0',
+    version='0.2.1',
     author='Jared Vasquez',
     url='https://github.com/jgv7/kubetemp',
     long_description='https://github.com/jgv7/kubetemp',
