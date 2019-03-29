1204c88ddd2d264f9de15652a7fc6e9ade5b3a1b
Jared Vasquez <jared.vasquez@cern.ch>
2019-03-29 16:48:35 -0700

    updated deployment process

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='kubetemp',
-    version='0.2.1',
+    version='0.2.2',
     author='Jared Vasquez',
     url='https://github.com/jgv7/kubetemp',
     long_description='https://github.com/jgv7/kubetemp',
