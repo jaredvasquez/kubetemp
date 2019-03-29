6ac6382f130587158fa00ecddceb9c493a1fb110
Jared Vasquez <jared.vasquez@yale.edu>
2019-03-29 16:51:04 -0700

    Added CLI testing (#9)
    Added CLI testing and automated deployments to PyPI

---
+++ kubetemp/cli.py
@@ -0,0 +1,33 @@
+import click
+
+from kubetemp import read_params, render_string, write_output
+
+
+@click.command()
+@click.argument('TEMPLATE', type=click.File('r'))
+@click.option(
+    'param_path', '--file', '-f', type=click.Path(exists=True),
+    help='Read parameters from JSON or YAML file'
+)
+@click.option(
+    'param_list', '--param', '-p', nargs=2, multiple=True, type=str,
+    help='Define parameters to render template'
+)
+@click.option(
+    'output_path', '--output', '-o', type=click.Path(writable=True),
+    help='Write rendered template to output file'
+)
+def _render_template(template, param_path, param_list, output_path):
+    """ Renders Jinja template with parameters"""
+    # Get template parameters
+    params = {}
+    file_params = read_params(param_path)
+    params.update(file_params)
+    params.update(dict(param_list))
+
+    # Render template and create output
+    rendered_output = render_string(template.read(), **params)
+    if output_path is None:
+        click.echo(rendered_output)
+    else:
+        write_output(rendered_output, output_path)
---
+++ tests/test_cli.py
@@ -0,0 +1,73 @@
+import uuid
+
+import click
+from click.testing import CliRunner
+import pytest
+
+from kubetemp.cli import _render_template
+
+
+@pytest.fixture
+def runner():
+    return CliRunner()
+
+
+@pytest.mark.parametrize('name', ['Joe', 'John', 'Jim', 123, ''])
+def test_param_arguments(runner, name):
+    result = runner.invoke(
+        _render_template,
+        ['tests/files/test.tmpl', '-p', 'name', name]
+    )
+    assert result.exit_code == 0
+    assert result.output == f'Hello, {name}!\n'
+
+
+@pytest.mark.parametrize('ext', ['json', 'yaml', 'yml'])
+def test_params_from_file(runner, ext):
+    result = runner.invoke(
+        _render_template,
+        ['tests/files/test.tmpl', '-f', f'tests/files/params.{ext}']
+    )
+    assert result.exit_code == 0
+    assert result.output == f'Hello, Tester!\n'
+
+
+@pytest.mark.parametrize('ext', ['json', 'yaml', 'yml'])
+def test_override_cli_params(runner, ext):
+    result = runner.invoke(
+        _render_template,
+        [
+            'tests/files/test.tmpl',
+            '-f', f'tests/files/params.{ext}',
+            '-p', 'name', 'Joe',
+        ]
+    )
+    assert result.exit_code == 0
+    assert result.output == f'Hello, Joe!\n'
+
+
+@pytest.mark.parametrize('name', ['Joe', 'John', 'Jim', 123, ''])
+def test_stdin_template(runner, name):
+    result = runner.invoke(
+        _render_template,
+        ['-', '-p', 'name', name],
+        input=f'How are you, {name}?'
+    )
+    assert result.exit_code == 0
+    assert result.output == f'How are you, {name}?\n'
+
+
+def test_write_output(runner, tmpdir):
+    path = tmpdir.join('test.txt')
+    hash_str = str(uuid.uuid4())
+    result = runner.invoke(
+        _render_template,
+        [
+            'tests/files/test.tmpl',
+            '-p', 'name', hash_str,
+            '-o', str(path)
+        ]
+    )
+    assert result.exit_code == 0
+    assert result.output == ''
+    assert path.read() == f'Hello, {hash_str}!'
